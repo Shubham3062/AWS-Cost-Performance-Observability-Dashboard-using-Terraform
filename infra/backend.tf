1ae469a90ec5cbcf8aea5f4bb684d9bcae1237f1
Shubham Shah <101097795+Shubham3062@users.noreply.github.com>
2025-08-19 14:49:41 +0100

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,1 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-project-backend-bucket"
+    key            = "state/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
 
