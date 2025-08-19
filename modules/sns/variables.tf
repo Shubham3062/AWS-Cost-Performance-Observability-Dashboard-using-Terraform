2e61031d2e75d2755c50dd24a55067ab1ef346ff
Shubham Shah <101097795+Shubham3062@users.noreply.github.com>
2025-08-19 19:34:32 +0100

    Update variables.tf

--- modules/sns/variables.tf
+++ modules/sns/variables.tf
@@ -1,1 +1,10 @@
+variable "project" {
+  description = "Project name prefix"
+  type        = string
+}
+
+variable "email" {
+  description = "Email address for SNS alerts"
+  type        = string
+}
 
