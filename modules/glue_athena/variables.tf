89c10993c2faf423d64818cf1ff41334a1ffa615
Shubham Shah <101097795+Shubham3062@users.noreply.github.com>
2025-08-19 19:33:21 +0100

    Update variables.tf

--- modules/glue_athena/variables.tf
+++ modules/glue_athena/variables.tf
@@ -1,1 +1,10 @@
+variable "project" {
+  description = "Project name prefix"
+  type        = string
+}
+
+variable "cur_bucket" {
+  description = "S3 bucket where CUR reports are delivered"
+  type        = string
+}
 
