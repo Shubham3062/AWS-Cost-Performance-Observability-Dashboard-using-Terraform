0721bd48d380fc49d820fdab14999a6b83ed9a5e
Shubham Shah <101097795+Shubham3062@users.noreply.github.com>
2025-08-19 15:03:26 +0100

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,15 +1,19 @@
 output "cur_bucket" {
-  value = module.cur.cur_bucket_name
+  value = aws_s3_bucket.cur_bucket.bucket
 }
 
-output "athena_db" {
-  value = module.glue_athena.athena_db
+output "glue_db" {
+  value = aws_glue_catalog_database.cur_db.name
 }
 
-output "lambda_function" {
-  value = module.lambda.lambda_name
+output "athena_query_results" {
+  value = aws_s3_bucket.athena_results.bucket
+}
+
+output "visualization_bucket" {
+  value = aws_s3_bucket.visualization_bucket.bucket
 }
 
 output "sns_topic" {
-  value = module.sns.sns_topic_arn
+  value = aws_sns_topic.cost_dashboard_topic.arn
 }
