d4a16cf16bd99e77607b1db7e99197adf275bc80
Shubham Shah <101097795+Shubham3062@users.noreply.github.com>
2025-08-19 18:59:31 +0100

    Create sns.tf

---
+++ modules/sns/sns.tf
@@ -0,0 +1,9 @@
+resource "aws_sns_topic" "cost_dashboard_topic" {
+  name = "${var.project}-sns"
+}
+
+resource "aws_sns_topic_subscription" "email_sub" {
+  topic_arn = aws_sns_topic.cost_dashboard_topic.arn
+  protocol  = "email"
+  endpoint  = "shahshubham130@gmail.com"
+}
