6dd06f57b934e790ea694d580e8ff82fb7b61a1a
Shubham Shah <101097795+Shubham3062@users.noreply.github.com>
2025-08-19 18:54:57 +0100

    Update lambda.tf

--- modules/lambda.tf
+++ modules/lambda.tf
@@ -1,1 +1,22 @@
+resource "aws_s3_bucket" "visualization_bucket" {
+  bucket        = "${var.project}-visualizations"
+  force_destroy = true
+}
 
+resource "aws_lambda_function" "cost_summary" {
+  function_name = "${var.project}-lambda"
+  filename      = "${path.module}/../lambda/cost_summary.zip"
+  handler       = "cost_summary.lambda_handler"
+  runtime       = "python3.9"
+}
+
+resource "aws_cloudwatch_event_rule" "daily_trigger" {
+  name                = "${var.project}-daily"
+  schedule_expression = "cron(0 8 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.daily_trigger.name
+  target_id = "lambda"
+  arn       = aws_lambda_function.cost_summary.arn
+}
