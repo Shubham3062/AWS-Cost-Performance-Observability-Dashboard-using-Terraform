b4d615a459bcb018dd019f48674ffffa52313584
Shubham Shah <101097795+Shubham3062@users.noreply.github.com>
2025-08-19 19:37:54 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,26 +1,23 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 module "cur" {
   source  = "../modules/cur"
-
+  project = var.project
 }
 
 module "glue_athena" {
-  source  = "../modules/glue_athena"
+  source     = "../modules/glue_athena"
+  project    = var.project
+  cur_bucket = module.cur.cur_bucket_name
 }
 
 module "lambda" {
-  source  = "../modules/lambda"
+  source                = "../modules/lambda"
+  project               = var.project
+  athena_db             = module.glue_athena.athena_db
+  athena_results_bucket = module.glue_athena.athena_results_bucket
 }
 
 module "sns" {
   source  = "../modules/sns"
+  project = var.project
+  email   = var.alert_email
 }
-
