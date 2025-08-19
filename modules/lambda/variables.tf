bb12ee1098fb3510b8eed1fd1c77c628506dcbaf
Shubham Shah <101097795+Shubham3062@users.noreply.github.com>
2025-08-19 19:34:01 +0100

    Update variables.tf

--- modules/lambda/variables.tf
+++ modules/lambda/variables.tf
@@ -1,1 +1,15 @@
+variable "project" {
+  description = "Project name prefix"
+  type        = string
+}
+
+variable "athena_db" {
+  description = "Athena database name"
+  type        = string
+}
+
+variable "athena_results_bucket" {
+  description = "S3 bucket for Athena query results"
+  type        = string
+}
 
