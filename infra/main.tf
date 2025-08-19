01079c2435df41b9830ec25353104f2e35df9bb7
Shubham Shah <101097795+Shubham3062@users.noreply.github.com>
2025-08-19 19:30:24 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -8,26 +8,19 @@ terraform {
 }
 
 module "cur" {
-  source  = "./cur"
-  project = var.project
+  source  = "../modules/cur"
+
 }
 
 module "glue_athena" {
-  source  = "./glue_athena"
-  project = var.project
-  cur_bucket = module.cur.cur_bucket_name
+  source  = "../modules/glue_athena"
 }
 
 module "lambda" {
-  source  = "./lambda"
-  project = var.project
-  athena_db = module.glue_athena.athena_db
-  athena_results_bucket = module.glue_athena.athena_results_bucket
+  source  = "../modules/lambda"
 }
 
 module "sns" {
-  source  = "./sns"
-  project = var.project
-  email   = var.alert_email
+  source  = "../modules/sns"
 }
 
